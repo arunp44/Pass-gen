1b4b165a53c0874d0a220d6026d44f701e91d7f1
arunp44 <137665388+arunp44@users.noreply.github.com>
2023-09-06 12:02:25 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 // Assignment code here
+var password=document.getElementById("password");
+
+ function generatePassword() {
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var passwordLength = 12;
+    var password = "";
+ for (var i = 0; i <= passwordLength; i++) {
+   var randomNumber = Math.floor(Math.random() * chars.length);
+   password += chars.substring(randomNumber, randomNumber +1);
+  }
+        document.getElementById("password").value = password;
+ }
+
+// function for password generdator from:
+//https://dev.to/code_mystery/random-password-generator-using-javascript-6a
 
 
 // Get references to the #generate element
@@ -9,7 +24,7 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = password.value;
 
 }
 
