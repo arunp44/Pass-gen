0b5341ac215bef76d54ceadd28cea30be760fc19
arunp44 <137665388+arunp44@users.noreply.github.com>
2024-03-04 12:38:23 -0700

    updates

--- script.js
+++ script.js
@@ -3,11 +3,63 @@ var password=document.getElementById("password");
 
  function generatePassword() {
     var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let pass = 7;
-    while (pass < 8) {
-      pass = prompt("Enter password lenght (8-128)", "Password")
-    }
-    ;
+    let pass = 6;
+    while (pass < 7) {
+      pass = prompt("Enter password lenght (8-128)", "Length")
+    };
+      function numbs() {
+        nums = prompt("Would you like the password to have numbers?", "nums")
+        // let answer = prompt("(yes or no)");
+        if (nums.toLowerCase() === "yes") {
+          chars = "0123456789";
+          specialchar();
+        } else if (nums.toLowerCase() === "no") {
+          chars = "";
+          specialchar();
+        }
+      }
+        
+        function specialchar() {
+        spchar = prompt("Would you like your password to have special chars", "(@#$%^&*())?")
+        // let answer2 = prompt(question + "(yes or no)");
+          if (spchar.toLowerCase() === "yes") {
+            chars = "!@#$%^&*()" + chars;
+            console.log(chars);
+            uppercase();
+          } else if (spchar.toLowerCase() === "no") {
+            // chars = "";
+            uppercase();
+          }
+        }
+
+        function uppercase() {
+        upcase = prompt("Would you like upper case letters in your password?", "UPPERCASE")
+        // let ans = prompt(question + "(yes or no)");
+          if (upcase.toLowerCase() === "yes") {
+            chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + chars;
+            lowcase();
+          } else if (upcase.toLowerCase() === "no") {
+            // chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()";
+            lowcase();
+          }
+        }
+
+        function lowcase() {
+        lcase = prompt("Would you like lower case letters in your password?", "lowercase")
+        // let answer3 = prompt(question + "(yes or no)");
+          if (lcase.toLowerCase() === "yes") {
+            chars = "abcdefghijklmnopqrstuvwxyz" + chars;
+          } else if (lcase.toLowerCase() === "no") {
+            // chars = "0123456789!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+          }
+        }
+      numbs()
+        if(!chars) {
+          alert("Must choose 1 option!!")
+          numbs()
+        }
+        console.log(chars)
+        
     let text;
     if (pass == null || pass == "") {
       text = "User cancelled the prompt.";
@@ -22,7 +74,7 @@ var password=document.getElementById("password");
    password += chars.substring(randomNumber, randomNumber +1);
   }
         document.getElementById("password").value = password;
- }
+     };
 
 // function for password generdator from:
 //https://dev.to/code_mystery/random-password-generator-using-javascript-6a
@@ -34,9 +86,9 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password.value;
+  // var passwordText = document.querySelector("#password");
+  // console.log(password);
+  // passwordText.value = password.value;
 
 }
 
